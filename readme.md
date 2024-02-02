ae1afe969cfebf2d6622c8c5d48d1c7f8e9f7f61
Daniel Layer <you@example.com>
2024-02-02 11:44:45 +0100

    firstcommit

---
+++ readme.md
@@ -0,0 +1,14 @@
+# DLTaster
+DLToaster ist eine leichte und benutzerfreundliche JavaScript-Bibliothek für die Implementierung von Toast-Benachrichtigungen in Webanwendungen. Mit DLToaster können Sie einfach und effektiv Toast-Nachrichten anzeigen, um Benutzer über bestimmte Ereignisse oder Informationen zu informieren.
+
+## Features
+position (top: left,right,center/ bottom: left,right,center)
+auto close (default 5 / as secounds / false)
+manuell close (true / false)
+append (before / after)
+textColor (css color)
+backgroundColor (css color / linear gradient)
+progressBar (true / false)
+progressColor (css color)
+border (true / false)
+icon
---
+++ toaster.js
@@ -0,0 +1,140 @@
+var DEFAULTVALUES = {
+    position: "top-right",
+    autoClose: 5,
+    manuallyClose: true,
+    append: "before",
+    textColor: "#000",
+    backgroundColor: "#fff",
+    progressColor: "linear-gradient(90deg, rgba(255,0,0,1) 20%, rgba(250,146,0,1) 40%, rgba(250,192,0,1) 60%, rgba(250,230,0,1) 80%, rgba(236,250,0,1) 100%)",
+    progressBar: false,
+    border: true,
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>'
+}
+
+export default class DLToaster{
+
+    #toaster
+    #intervall
+    #append
+    #progressIntervall
+
+    constructor(options){
+        this.#toaster = document.createElement("div");
+        this.#toaster.classList.add("dl-toast");
+        
+        var infoContainer = document.createElement("div");
+        infoContainer.classList.add("dl-toast-info-container");
+        infoContainer.style.color = options.textColor || DEFAULTVALUES.textColor;
+        infoContainer.style.backgroundColor = options.backgroundColor || DEFAULTVALUES.backgroundColor;
+        
+        var icon = document.createElement("div");
+        icon.classList.add("dl-toast-icon");
+        icon.innerHTML = options.icon || DEFAULTVALUES.icon;
+        
+        var textContainer = document.createElement("div");
+        textContainer.classList.add("dl-toast-text-container");
+        var mainText = document.createElement("div");
+        mainText.classList.add("dl-toast-text-main");
+        var detailText = document.createElement("div");
+        detailText.classList.add("dl-toast-text-detail");
+        textContainer.appendChild(mainText);
+        textContainer.appendChild(detailText);
+
+        var dissmis = document.createElement("div");
+        dissmis.classList.add("dl-toast-dismiss");
+        dissmis.innerHTML = "&times;";
+
+        infoContainer.appendChild(icon);
+        infoContainer.appendChild(textContainer);
+        infoContainer.appendChild(dissmis);
+
+        var progressContainer = document.createElement("div");
+        progressContainer.classList.add("dl-toast-progress-bar-container");
+        var progressBar = document.createElement("div");
+        progressBar.classList.add("dl-toast-progress-bar");
+        progressBar.style.background = options.progressColor || DEFAULTVALUES.progressColor;
+        progressContainer.appendChild(progressBar);
+
+        this.#toaster.appendChild(infoContainer);
+        if(options.progressBar === true){
+            this.#toaster.appendChild(progressContainer);
+        }
+
+        if(options.border === false){
+            this.#toaster.style.border = "none"
+        }
+
+        this.#append = options.append || DEFAULTVALUES.append;
+
+        Object.entries({...DEFAULTVALUES, ...options}).forEach(([key,value]) =>{            
+            this[key] = value
+        });
+    }
+
+    set position(value){
+        var selector = `.dl-toast-container[data-position=${value}]`,
+        container = document.querySelector(selector) || createContainer(value);
+
+        if(this.#append == "before"){
+            container.insertBefore(this.#toaster,container.firstChild);
+        }else{
+            container.append(this.#toaster);
+        }
+        
+    }
+
+    set mainText(value){
+        this.#toaster.querySelector(".dl-toast-text-main").textContent = value;
+    }
+
+    set detailText(value){
+        this.#toaster.querySelector(".dl-toast-text-detail").textContent = value;
+    }
+
+    set autoClose(value){
+        if(value === false){return}
+        var setDate = new Date();
+        this.#progressIntervall = setInterval(()=>{
+            var x = (100-(((((setDate - new Date() ) / 1000) * (-1)) / value )*100)).toString();
+            this.#toaster.querySelector(".dl-toast-progress-bar").style.width = x + "%";
+        },10)
+        this.#intervall = setTimeout(()=>{
+            this.remove();
+        },value*1000)
+    }
+
+    set manuallyClose(value){
+        if(value === false){
+            this.#toaster.querySelector(".dl-toast-dismiss").remove();
+            return
+        }
+
+        var c = this;
+        this.#toaster.addEventListener("click",function(){
+            clearTimeout(c.#intervall);
+            c.remove();
+        })
+    }
+
+    remove(){
+        const container = this.#toaster.parentElement;
+        this.#toaster.classList.add("closeing"); 
+        const event = new CustomEvent("dlToast:close",{detail: this.#toaster});
+        document.dispatchEvent(event);
+        setTimeout(()=>{
+            this.#toaster.remove();
+            clearInterval(this.#progressIntervall);
+            if(container != null && !container.hasChildNodes()){
+                container.remove();
+            }
+        },500)
+    }
+}
+
+function createContainer(position){
+    const container = document.createElement("div");
+    container.classList.add("dl-toast-container");
+    container.dataset.position = position;
+    document.body.append(container);
+    return container;
+}
